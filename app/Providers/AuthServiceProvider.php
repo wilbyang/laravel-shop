263977d063a22319afa634fb2bfc4a4367d45b86
yang bo <yang.wilby@gmail.com>
2018-07-25 11:47:53 +0200

    api set up using passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,7 @@ use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -29,7 +30,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes();
+        Passport::tokensExpireIn(now()->addDays(15));
 
+        Passport::refreshTokensExpireIn(now()->addDays(30));
         //
     }
 }
